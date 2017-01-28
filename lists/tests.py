5487d9cc42d69e209a239b414a0c1627b163db34
Michael Woon <woon@mit.edu>
2017-01-28 18:04:43 +0100

    Redirect after POST, retain all POSTed items and display in template

--- lists/tests.py
+++ lists/tests.py
@@ -14,8 +14,31 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'a new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'a new list item')
+
+    def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'a new list item'})
-        self.assertIn('a new list item', response.content.decode())
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='thingy 1')
+        Item.objects.create(text='thingy 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('thingy 1', response.content.decode())
+        self.assertIn('thingy 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
 

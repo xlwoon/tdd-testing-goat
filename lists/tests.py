db7e2096b22e7497260f7a9d8cdde63bddc7a37d
Michael Woon <woon@mit.edu>
2017-01-28 16:53:06 +0100

    Added model for list Items, made migrations

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
+from lists.models import Item
 from lists.views import home_page
 
 # Create your tests here.
@@ -15,3 +16,22 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'a new list item'})
         self.assertIn('a new list item', response.content.decode())
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Second item, directly after first'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual('The first (ever) list item', first_saved_item.text)
+        self.assertEqual('Second item, directly after first', second_saved_item.text)

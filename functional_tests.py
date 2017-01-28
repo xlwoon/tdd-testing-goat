4c101828e3e28491833dad998e35700a39a50047
Michael Woon <woon@mit.edu>
2017-01-28 11:06:45 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.keys import Keys
+
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,17 +20,29 @@ class NewVisitorTest(unittest.TestCase):
 
     # she notices it indicates clearly it's a to-do app
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
     # she is immediately invited to add an item to the to-do list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual( inputbox.get_attribute('placeholder'), 'Enter a to-do item' )
 
     # she types "buy frozen pizza" into a text box
+        inputbox.send_keys('Buy frozen pizza')
     # Uma loves frozen pizza
 
     # when she submits the contents, the page updates and lists
     # "1: buy frozen pizza" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue( any(row.text == '1: Buy frozen pizza' for row in rows) )
+
 
     # the updated page also contains a text box for further items to be added
+        self.fail('Finish the test')
+
 
     # Uma adds "eat frozen pizza" into the text box
     # she doesn't want to forget

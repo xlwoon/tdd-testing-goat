15a9d6edad06b9cb41cd2afd0b82a69a4632bb71
Michael Woon <woon@mit.edu>
2017-01-28 16:08:58 +0100

    Refactor checking for row in list table using helper method

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
     # Uma has heard about this cool new website, an automated to-do list
     # she fires up a browser to check out it's home page
@@ -35,9 +41,7 @@ class NewVisitorTest(unittest.TestCase):
     # "1: buy frozen pizza" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy frozen pizza', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy frozen pizza')
 
     # the updated page also contains a text box for further items to be added
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -49,10 +53,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
     # when the page updates again, it contains both items
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy frozen pizza', [row.text for row in rows])
-        self.assertIn('2: Eat Frozen Pizza', [row.text for row in rows])
+
+        self.check_for_row_in_list_table('1: Buy frozen pizza')
+        self.check_for_row_in_list_table('2: Buy frozen pizza')
 
     # Uma wonders if the site will remember her list
     # it does: and generates a unique URL for it

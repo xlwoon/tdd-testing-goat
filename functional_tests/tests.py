6481f3555349a8b186ef5c38cbf2b29da6490aab
Michael Woon <woon@mit.edu>
2017-01-30 16:25:21 +0100

    Add tests for multiple users and saving their lists to unique URLs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -59,9 +59,48 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('2: Eat frozen pizza')
 
     # Uma wonders if the site will remember her list
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy frozen pizza')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy frozen pizza')
+
     # it does: and generates a unique URL for it
+        uma_list_url = self.browser.current_url
+        self.assertRegex(uma_list_url, '/lists/.+')
+
+
+    # a new user, Ulrich comes along.
+    ## we use a new browser session to make sure none of Uma's data is coming through from cookies
+        self.browser.quit()
+        self.browser = webdriver.Firefox(firefox_binary = FirefoxBinary(firefox_path = '/home/woon/Documents/learn/TDD/testing-goat/firefox/firefox'))
+        self.browser.implicitly_wait(5)
+
+    # Ulrich visits homepage and sees no trace of Uma's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy frozen pizza', page_text)
+        self.assertNotIn('Eat frozen pizza', page_text)
+
+    # Ulrich starts his own list by entering items
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy eggs')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy eggs')
+
+    # Ulrich's list gets a unique URL
+        ulrich_list_url = self.browser.current_url
+        self.assertRegex(ulrich_list_url, '/lists/.+')
+        self.assertNotEqual(ulrich_list_url, uma_list_url)
+
+    # There is still no trace of Uma's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy frozen pizza', page_text)
+        self.assertNotIn('Eat frozen pizza', page_text)
+
     # also displays some explanatory text about that
-        self.fail('Finish the test!')
 
     # she double checks the URL works, and that her list is displayed
 
